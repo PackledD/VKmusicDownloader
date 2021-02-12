34b2adc36f2eaf204bfcff420eefa930b8aac99d
PackledD <andrewklad@yandex.ru>
2021-02-12 21:04:22 +0300

    Made GUI v1.0 for mainfile. Microfix of logging

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from log import Logging
 
 
 try:
-    user = auth.auth('', '')
+    user = auth.auth('89092804682', 'VKqwerty228')
     user.session_start()
     music.get_songs_info(user)
-    # music.download_song('Gloryhammer - Apocalypse 1992', user)
+    music.download_song('Gloryhammer - Apocalypse 1992', user)
 except KeyboardInterrupt:
     Logging("Stop programm by yourself", user.logfile)
     raise SystemExit
