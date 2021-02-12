8dd098dae525c207c7590666e51ab513a2e50a73
PackledD <andrewklad@yandex.ru>
2021-02-12 20:55:51 +0300

    Made GUI v1.0 for main file (mainroot). Microfix of logging

--- libs/log.py
+++ libs/log.py
@@ -19,18 +19,21 @@ def clear_last_log():  # Clear content of file "last_log.txt"
 
 
 def Logging(text, file):
-    '''
-    This function make logging something
-    It take two arguments: text which woill place in logfile and logfile name
-    Also it dupplicate new log inta "last_log.txt"
-    '''
-    if not os.path.exists("./log"):
-        os.mkdir("log")
-    with open("log/" + file, 'a') as f:
-        f.write('[{0}][{1}]'.format(get_formated_date(),
-                                    get_formated_time()) + text + '\n')
-    if file != 'last_log.txt':
-        Logging_last_log(text)
+    try:
+        '''
+        This function make logging something
+        It take two arguments: text which woill place in logfile and logfile name
+        Also it dupplicate new log inta "last_log.txt"
+        '''
+        if not os.path.exists("./log"):
+            os.mkdir("log")
+        with open("log/" + file, 'a') as f:
+            f.write('[{0}][{1}]'.format(get_formated_date(),
+                                        get_formated_time()) + text + '\n')
+        if file != 'last_log.txt':
+            Logging_last_log(text)
+    except Exception:
+        pass
 
 
 def Logging_last_log(text):
--- libs/music.py
+++ libs/music.py
@@ -3,7 +3,6 @@ import os
 from log import Logging
 import requests
 import eyed3
-# import shutil
 
 
 def make_name_normal(a):
@@ -16,7 +15,7 @@ def make_name_normal(a):
 def get_songs_info(user):
     '''
     This function get all info about user music
-    It put these into JSON-files (data files)
+    It put its into JSON-files (data files)
     If song has bad decoding name or author (not-unicode), its put
     it in folder "Bad_Songs" with song ID as name
     Else its put it in folder "Normal_Songs" (Name = 'Author' - 'Title')
